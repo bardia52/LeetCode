f698305512b21402febeaa52686b15cc0332e828
Bardia Alavi <bardia_alavi@yahoo.com>
2018-07-22 23:01:43 -0400

    change tab to space

--- Algorithms/two_sum.cpp
+++ Algorithms/two_sum.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
-	vector<int> twoSum(vector<int>& nums, int target) {
-		int len = nums.size();
-		vector<int> ret_vector(2);
-		//vector<int> ret_vector = [0, 0];
-		for (int i = 0; i < len - 1; i++) {
-			for (int j = i + 1; j < len; j++) {
-				if (nums[i] + nums[j] == target) {
-					ret_vector[0] = i;
-					ret_vector[1] = j;
-					break;
-				}
-			}
-		}
-		return ret_vector;
-	}
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int len = nums.size();
+        vector<int> ret_vector(2);
+        //vector<int> ret_vector = [0, 0];
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ret_vector[0] = i;
+                    ret_vector[1] = j;
+                    break;
+                }
+            }
+        }
+        return ret_vector;
+    }
 };
