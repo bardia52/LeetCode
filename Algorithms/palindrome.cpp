9b028f96ad12d90b54bc4f36928548f5a79a52b7
Bardia Alavi <bardia_alavi@yahoo.com>
2018-07-23 23:51:31 -0400

    add solution to palindrome
    This solution is not optimal. It is reversing the complete number instead of half of the number.

---
+++ Algorithms/palindrome.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        // Check for negative
+        if (x < 0)
+            return false;
+        // Check if equal to reverse
+        if ( x == reverse(x))
+            return true;
+        else
+            return false;
+    }
+private:
+    int reverse(int x) {
+        // Getting digits one by one
+        int x_tmp = x;
+        vector<int> digits(10);
+        int digit_inx = 0;
+        while (x_tmp) {
+            digits[digit_inx] = x_tmp % 10;
+            x_tmp /= 10;
+            digit_inx++;
+        }
+
+        // Constructing reverse number one digit at a time
+        int rev_int = 0;
+        for (int i = digit_inx; i > 0 ; i--) {
+            int temp = rev_int * 10;
+            // Overflow Check
+            if (temp / 10 != rev_int)
+                return 0; // Overflow
+            else
+                rev_int = temp;
+            temp = rev_int + digits[digit_inx-i];
+            // Overflow Check
+            if (temp - digits[digit_inx-i] != rev_int)
+                return 0; // Overflow
+            else
+                rev_int = temp;
+        }
+        return rev_int;
+    }
+};
