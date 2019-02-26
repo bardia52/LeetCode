8b7fea900613b6c79fa8d7d56a63dd3d6e5c41f3
Bardia Alavi <bardia_alavi@yahoo.com>
2019-02-25 20:02:47 -0500

    Final - The fastest python solution

--- Algorithms/spiral_matrix.py
+++ Algorithms/spiral_matrix.py
@@ -15,21 +15,19 @@ class Solution(object):
         colR = n-1
 
         while (rowUp <= rowDown) and (colL <= colR):
-            print rowUp, rowDown, colL, colR
-            print "rowUp", rowUp, ": ", matrix[rowUp][colL:colR+1]
-            res.extend(matrix[rowUp][colL:colR])
-            for row in range(rowUp,rowDown+1):
-                print "colR", colR, ": ", matrix[row][colR]
-                res.append(matrix[row][colR])
-            print "rowDown", rowDown, ": ", matrix[rowDown][colL+1:colR][::-1]
-            res.extend(matrix[rowDown][colL+1:colR][::-1])
-            for row in range(rowDown,rowUp,-1):
-                print "colL", colL, ": ", matrix[row][colL]
-                res.append(matrix[row][colL])
-            rowUp += 1
-            colR -= 1
-            rowDown -= 1
-            colL += 1
-        print rowUp, rowDown, colL, colR
-            
+            if (rowUp <= rowDown) and (colL <= colR):
+                res.extend(matrix[rowUp][colL:colR+1])
+                rowUp += 1
+            if (rowUp <= rowDown) and (colL <= colR):
+                for row in range(rowUp,rowDown+1):
+                    res.append(matrix[row][colR])
+                colR -= 1
+            if (rowUp <= rowDown) and (colL <= colR):
+                res.extend(matrix[rowDown][colL:colR+1][::-1])
+                rowDown -= 1
+            if (rowUp <= rowDown) and (colL <= colR):
+                for row in range(rowDown,rowUp-1,-1):
+                    res.append(matrix[row][colL])
+                colL += 1
+
         return res
