c5195343f16ea078f4f5eccd072e496c4588660a
Bardia Alavi <bardia_alavi@yahoo.com>
2019-03-09 12:26:53 -0500

    Create game_of_life.py

---
+++ Algorithms/game_of_life.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def gameOfLife(self, board):
+        """
+        :type board: List[List[int]]
+        :rtype: None Do not return anything, modify board in-place instead.
+        """
+        m = len(board)
+        n = len(board[0])
+        temp = [[0]*n for i in range(m)]
