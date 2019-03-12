df48882dc2384c4f07ed4400c5bb30aa097e2914
Bardia Alavi <bardia_alavi@yahoo.com>
2019-03-11 23:49:14 -0400

    Create moving_average_from_data_stream.py

---
+++ Algorithms/moving_average_from_data_stream.py
@@ -0,0 +1,31 @@
+class MovingAverage(object):
+
+    def __init__(self, size):
+        """
+        Initialize your data structure here.
+        :type size: int
+        """
+        self.array = [0]*size
+        self.pointer = 0
+        self.sizeMet = False
+        self.ave = float(0)
+        self.size = size
+
+    def next(self, val):
+        """
+        :type val: int
+        :rtype: float
+        """
+        # print self.pointer, self.array
+        oldVal = self.array[self.pointer]
+        self.array[self.pointer] = val
+        self.pointer += 1
+        if self.pointer == self.size:
+            self.pointer = 0
+            self.sizeMet = True
+        if not self.sizeMet:
+            self.ave = float(sum(self.array[0:self.pointer])) / float(self.pointer)
+        else:
+            self.ave += (float(val-oldVal) / float(self.size))
+            self.ave = float(sum(self.array)) / float(self.size)
+        return self.ave
