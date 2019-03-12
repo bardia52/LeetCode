371f0d864a8c5e98ebfae9c31d5d2573888a4b57
Bardia Alavi <bardia_alavi@yahoo.com>
2019-03-12 00:04:55 -0400

    Update moving_average_from_data_stream.py

--- Algorithms/moving_average_from_data_stream.py
+++ Algorithms/moving_average_from_data_stream.py
@@ -16,13 +16,18 @@ class MovingAverage(object):
         :type val: int
         :rtype: float
         """
+        oldVal = self.array[self.pointer]
         self.array[self.pointer] = val
+        if not self.sizeMet:
+            self.ave = float(sum(self.array[0:self.pointer+1])) / float(self.pointer+1)
+        else:
+            self.ave += (float(val-oldVal) / float(self.size))
         self.pointer += 1
         if self.pointer == self.size:
             self.pointer = 0
             self.sizeMet = True
-        if not self.sizeMet:
-            self.ave = float(sum(self.array[0:self.pointer])) / float(self.pointer)
-        else:
-            self.ave = float(sum(self.array)) / float(self.size)
         return self.ave
+
+# Your MovingAverage object will be instantiated and called as such:
+# obj = MovingAverage(size)
+# param_1 = obj.next(val)
